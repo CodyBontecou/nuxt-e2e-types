4629a97fb6838f8edce552349e17e090d6915977
Cody Bontecou <bontecouc@gmail.com>
2024-02-27 22:48:39 -0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { User } from './types/user'
+
+const { data: userData } = await useFetch<User>('/api/test')
+</script>
+
+<template>
+  <pre>
+    {{ userData }}
+  </pre>
+</template>
---
+++ server/api/test.ts
@@ -0,0 +1,5 @@
+import type { User } from '~/types/user'
+
+export default defineEventHandler(() => {
+  return { test: '0' } as User
+})

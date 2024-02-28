a63cf51e0e9831a78905f3fff24b4a71e729d351
Cody Bontecou <bontecouc@gmail.com>
2024-02-28 11:53:33 -0700

    cleaning up naming

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { User } from './types/user'
 
-const { data: userData } = await useFetch<User>('/api/test')
+const { data: userData } = await useFetch<User>('/api/getUsers')
 </script>
 
 <template>
--- server/api/test.ts
+++
@@ -1,5 +0,0 @@
-import type { User } from '~/types/user'
-
-export default defineEventHandler(() => {
-  return { test: '0' } as User
-})
